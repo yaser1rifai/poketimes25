c0f42ea3f1dbd791bb4b85f9ec70e639c66b08c7
yaser1rifai <yaser1rifai@gmail.com>
2019-10-21 22:29:20 +0200

    Updates

---
+++ precache-manifest.1714c3a151ca006d301c53973792035e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2947d405ad9da3e4f6e9870d187c1578",
+    "url": "/poketimes25/index.html"
+  },
+  {
+    "revision": "f416b0453f6d7dbd8bce",
+    "url": "/poketimes25/static/css/main.edbb531f.chunk.css"
+  },
+  {
+    "revision": "39a5eb0948649cf90d2c",
+    "url": "/poketimes25/static/js/2.d14b1ea2.chunk.js"
+  },
+  {
+    "revision": "f416b0453f6d7dbd8bce",
+    "url": "/poketimes25/static/js/main.5e1f65d7.chunk.js"
+  },
+  {
+    "revision": "775f9d539901dca10211",
+    "url": "/poketimes25/static/js/runtime~main.9b397d66.js"
+  }
+]);
